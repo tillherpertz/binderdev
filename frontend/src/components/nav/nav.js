eec0485fa2c422b26eb7445c9cf2932d1d83ebe5
tillherpertz <tillh@live.de>
2022-11-14 19:38:56 +0100

    added mobile nav scripts and some mobile styling

--- frontend/src/components/nav/nav.js
+++ frontend/src/components/nav/nav.js
@@ -4,49 +4,80 @@ import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 
 import CAT_QUERY from "../../queries/categories_query";
+import { recallCache } from "@apollo/client/cache/inmemory/reactiveVars";
 
-function Nav() {
+class Nav extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            navOpen: false
+        }
+    }
 
-    return (
-        <div className="navigation-wrapper">
-            <button className="icon--toggle-menu" type="button">
-                <span></span>
-                <span></span>
-            </button>
-            <div className="collapsed" id="navbar">
-                <Query query={CAT_QUERY} id={null}>
-                    {({ data: { categories } }) => {
-                        return (
-                            <nav>
-                                <ul>
-                                    <li key="home">
-                                        <NavLink
-                                            to="/"
-                                            end
-                                            className={({ isActive }) => (isActive ? 'active' : undefined)}>
-                                            home.
-                                        </NavLink>
-                                    </li>
-                                    {categories.data.map((category) => {
-                                        return (
-                                            <li key={category.attributes.Slug}>
-                                                <NavLink
-                                                    to={'/' + category.attributes.Slug}
-                                                    className={({ isActive }) => (isActive ? 'active' : undefined)}>
-                                                    {category.attributes.Name}.
-                                                </NavLink>
-                                            </li>
+    navToggle() {
+        const idNavbar = document.getElementById('navbar');
+        const classIconToggle = document.getElementsByClassName('icon--toggle-menu')[0];
+        if (this.state.navOpen === false) {
+            this.setState({
+                navOpen: true
+            })
+            idNavbar.classList.add('show');
+            classIconToggle.classList.toggle('open');
+        } else {
+            this.setState({
+                navOpen: false
+            })
+            idNavbar.classList.remove('show');
+            classIconToggle.classList.toggle('open');
+        }
+    }
+    render() {
+        return (
+            <div className="navigation-wrapper">
+                <button
+                    className="icon--toggle-menu"
+                    type="button"
+                    onClick={() => {
+                        this.navToggle()
+                    }}>
+                    <span></span>
+                    <span></span>
+                </button>
+                <div className="collapsed" id="navbar">
+                    <Query query={CAT_QUERY} id={null}>
+                        {({ data: { categories } }) => {
+                            return (
+                                <nav>
+                                    <ul>
+                                        <li key="home">
+                                            <NavLink
+                                                to="/"
+                                                end
+                                                className={({ isActive }) => (isActive ? 'active' : undefined)}>
+                                                home.
+                                            </NavLink>
+                                        </li>
+                                        {categories.data.map((category) => {
+                                            return (
+                                                <li key={category.attributes.Slug}>
+                                                    <NavLink
+                                                        to={'/' + category.attributes.Slug}
+                                                        className={({ isActive }) => (isActive ? 'active' : undefined)}>
+                                                        {category.attributes.Name}.
+                                                    </NavLink>
+                                                </li>
 
-                                        );
-                                    })}
-                                </ul>
-                            </nav>
-                        );
-                    }}
-                </Query >
+                                            );
+                                        })}
+                                    </ul>
+                                </nav>
+                            );
+                        }}
+                    </Query >
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default Nav
