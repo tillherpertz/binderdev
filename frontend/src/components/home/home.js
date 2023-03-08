2d9b0ff32536f6764547c07dcd16856dc1b869fd
tillherpertz <tillh@live.de>
2023-03-08 16:32:06 +0100

    home animation

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -3,8 +3,22 @@ import Query from "../Query/query";
 import Header from '../header/header'
 import Footer from '../footer/footer'
 import HOME_QUERY from "../../queries/home_query";
+import { useState } from "react";
+import { useInView } from 'react-intersection-observer';
 
 function Home() {
+
+    const [hasBeenInView, setHasBeenInView] = useState(false);
+
+    const { ref, inView } = useInView({
+        skip: hasBeenInView,
+    });
+
+    // Update hasBeenInView to skip element
+    if (inView && !hasBeenInView) {
+        setHasBeenInView(true);
+    }
+
     return (
         <div>
             <Header></Header>
@@ -12,10 +26,10 @@ function Home() {
                 <Query query={HOME_QUERY}>
                     {({ data: { homepage } }) => {
                         return (
-                            <div>
-                                <h1 className="hero-headline">{homepage.data.attributes.Headline}</h1>
-                                <p className="hero-paragraph">{homepage.data.attributes.Paragraph}</p>
-                                <div className="button-wrap">
+                            <div className={`home-content`}>
+                                <h1 ref={ref} className={`hero-headline ${inView ? 'animate-show' : ''}`}>{homepage.data.attributes.Headline}</h1>
+                                <p ref={ref} className={`hero-paragraph ${inView ? 'animate-show' : ''}`}>{homepage.data.attributes.Paragraph}</p>
+                                <div ref={ref} className={`button-wrap ${inView ? 'animate-show' : ''}`}>
                                     <a href="/portfolio"><button>portfolio.</button></a>
                                     <a href="/blog"><button>blog.</button></a>
                                 </div>
